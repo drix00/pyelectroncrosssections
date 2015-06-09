998c68bd0474d8c02de85417b54b2fc05e1af714
Hendrix Demers <12611589+drix00@users.noreply.github.com>
2015-06-09 19:24:59 -0400

    Add module to read ELSEAP cross section from the zip file.

--- pyElectronCrossSections/ElsepaFigures.py
+++ pyElectronCrossSections/ElsepaFigures.py
@@ -22,16 +22,24 @@ import numpy as np
 
 # Local modules.
 import DatabasesTools.Casino3.ElsepaBinaryFile as ElsepaBinaryFile
+from pyElectronCrossSections.Models.elsepa_casino import ElsepaCasino
+from pyElectronCrossSections import current_module_path
 
 # Globals and constants variables.
 
 def run():
     path = r"D:\work\data\Casino3\ELDB"
+    zipfilepath = current_module_path(__file__, "../testdata/ELSEPA_AbsorptionCorrection_LinearInterpolationTabulation_0.1.zip")
 
     atomicNumbers = range(1, 99+1)
     totalCS_nm2 = {}
+    totalCS2_nm2 = {}
+
+    elsepa_casino = ElsepaCasino(zipfilepath)
     for atomicNumber in atomicNumbers:
         totalCS_nm2[atomicNumber] = {}
+        totalCS2_nm2[atomicNumber] = {}
+
         filename = "EL%i.els" % (atomicNumber)
         filepath = os.path.join(path, filename)
         elsepaFile = ElsepaBinaryFile.ElsepaBinaryFile(filepath)
@@ -42,26 +50,37 @@ def run():
 
             totalCS_nm2[atomicNumber][energy_keV] = total
 
+            energy_eV = energy_keV * 1.0e3
+            total_nm2 = elsepa_casino.total_nm2(atomicNumber, energy_eV)
+            totalCS2_nm2[atomicNumber][energy_keV] = total_nm2
+
     energy_keV = 20.0
     y = []
+    y2 = []
     for atomicNumber in atomicNumbers:
         y.append(totalCS_nm2[atomicNumber][energy_keV])
+        y2.append(totalCS2_nm2[atomicNumber][energy_keV])
 
     plt.figure()
     plt.title(r"$E_{0} = %.1f$" % (energy_keV))
     plt.plot(atomicNumbers, y)
+    plt.plot(atomicNumbers, y2, '.')
+
     plt.xlabel('Z')
     plt.ylabel(r"$\sigma_{T}$ (nm$^{2}$)")
 
     energies_keV = sorted(totalCS_nm2[atomicNumber].keys())
     atomicNumber = 29
     y = []
+    y2 = []
     for energy_keV in energies_keV:
         y.append(totalCS_nm2[atomicNumber][energy_keV])
+        y2.append(totalCS2_nm2[atomicNumber][energy_keV])
 
     plt.figure()
     plt.title("Z = %i" % (atomicNumber))
     plt.loglog(energies_keV, y)
+    plt.loglog(energies_keV, y2, '.')
     plt.xlabel('energy (keV)')
     plt.ylabel(r"$\sigma_{T}$ (nm$^{2}$)")
 
